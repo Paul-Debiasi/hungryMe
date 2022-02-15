dbfb6d23bf303c608097f03768a0b223c8b9dff0
Simona <semuona@gmail.com>
2022-02-15 16:26:16 +0100

    created branch

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,12 @@ import "./App.scss";
 import Header from "./components/Header/Header";
 import Main from "./components/Main/Main";
 function App() {
-	return (
-		<div className='App'>
-			<Header />
-			<Main />
-		</div>
-	);
+  return (
+    <div className="App">
+      <Header />
+      <Main />
+    </div>
+  );
 }
 
 export default App;
