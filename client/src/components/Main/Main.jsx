3b758c3b6f959e92bbaa0c744ce0ace5b4895f89
Simona <semuona@gmail.com>
2022-02-16 15:33:28 +0100

    log in works

--- client/src/components/Main/Main.jsx
+++ client/src/components/Main/Main.jsx
@@ -3,16 +3,17 @@ import { HungryMeContext } from "../../Context";
 import Card from "../Card/Card";
 
 export default function Main() {
-	const { menu } = useContext(HungryMeContext);
+  const { menu } = useContext(HungryMeContext);
 
-	return (
-		<div>
-			<h1>Restaurants</h1>
-			<div className='cardContainer'>
-				{menu.map((item) => (
-					<Card item={item} />
-				))}
-			</div>
-		</div>
-	);
+  return (
+    <div>
+      <h1>Restaurants</h1>
+
+      <div className="cardContainer">
+        {menu.map((item, idx) => (
+          <Card item={item} key={idx} />
+        ))}
+      </div>
+    </div>
+  );
 }
