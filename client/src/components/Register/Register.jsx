9c580ef4283eef4e81cc62124866edf5fb7f2823
Paul Debiasi <debiasi.paul@gmail.com>
2022-02-18 07:30:33 +0100

    saving chages

--- client/src/components/Register/Register.jsx
+++ client/src/components/Register/Register.jsx
@@ -2,16 +2,22 @@ import { useState, useContext, useEffect } from "react";
 import { v4 as uuidv4 } from "uuid";
 import { HungryMeContext } from "../../Context";
 import axios from "axios";
+import { Link, useHistory } from "react-router-dom";
 
 export default function Register() {
-	const { menu, setMenu, clientUser, setClientUser } =
-		useContext(HungryMeContext);
+	const {
+		menu,
+		setMenu,
+		clientUser,
+		setClientUser,
+		businessUser,
+		setBusinessUser,
+	} = useContext(HungryMeContext);
 	const [business, setBusiness] = useState(false);
 	const [client, setClient] = useState(false);
 	const [user, setUser] = useState({});
-	console.log(user);
-	console.log("Client ", client);
-	console.log("business", business);
+	const [val, setVal] = useState();
+	let history = useHistory();
 
 	const handleClient = (e) => {
 		e.preventDefault();
@@ -25,16 +31,36 @@ export default function Register() {
 		});
 	};
 
-	const submitClient = async (e) => {
-		e.preventDefault();
-
-		const data = clientUser;
-		console.log("Submiting !!", data);
-		const response = await axios.post("/register", data);
-		console.log(response);
+	const clear = (e) => {
+		e.target.value = "";
+		console.log("From clear", e.target);
+	};
+	const handleBusiness = (e) => {
+		setBusinessUser({
+			...businessUser,
+			id: uuidv4(),
+			business: true,
+			[e.target.name]: e.target.value,
+		});
+		console.log(businessUser);
 	};
 
-	console.log(clientUser);
+	const submitClient = async (e) => {
+		e.preventDefault();
+		if (val === "") return;
+		const data = { ...clientUser };
+		console.log("Submiting !!", data);
+		const response = await axios.post("/clients", data);
+		console.log(response);
+		setVal(() => "");
+	};
+	const submitBusiness = async (e) => {
+		e.preventDefault();
+		const data = businessUser;
+		const response = await axios.post("/business", data);
+		console.log(response);
+	};
+	console.log(val);
 
 	return (
 		<div>
@@ -65,44 +91,115 @@ export default function Register() {
 				}}
 			/>{" "}
 			Client
-			<form>
-				<label className='label'>Name</label>
-				<input
-					onChange={handleClient}
-					className='input'
-					name='username'
-					type='text'
-					id='name'
-				/>
-				<label className='label'>Email</label>
-				<input
-					onChange={handleClient}
-					className='input'
-					type='email'
-					name='email'
-				/>
-				<label className='label'>Password</label>
-				<input
-					onChange={handleClient}
-					className='input'
-					type='password'
-					name='password'
-				/>
-				{client ? (
+			{client ? (
+				<form>
+					<label className='label'>Name</label>
+					<input
+						onChange={handleClient}
+						className='input'
+						name='username'
+						type='text'
+						id='name'
+						value={val}
+					/>
+					<label className='label'>Email</label>
+					<input
+						onChange={handleClient}
+						className='input'
+						type='email'
+						name='email'
+						value={val}
+					/>
+					<label className='label'>Password</label>
+					<input
+						onChange={handleClient}
+						className='input'
+						type='password'
+						name='password'
+						value={val}
+					/>
+
 					<button className='btn' type='submit' onClick={submitClient}>
 						Submit
 					</button>
-				) : (
+				</form>
+			) : null}
+			{business ? (
+				<form
+					style={{
+						display: "flex",
+						flexDirection: "column",
+						justifyContent: "center",
+					}}
+				>
 					<>
-						<label className='label'>Address</label>
-						<input type='text' name='address' id='address' />
-						<label className='label'>Neighbor</label>
-						<input type='text' name='neighbor' id='neighbor' />
-						<label className='label'>Cuisine</label>
-						<input type='text' name='cuisine' id='cuisine' />
+						<div>
+							<label className='label'>Name</label>
+							<input
+								onChange={handleBusiness}
+								className='input'
+								name='username'
+								type='text'
+								id='name'
+							/>
+						</div>
+						<div>
+							<label className='label'>Email</label>
+							<input
+								onChange={handleBusiness}
+								className='input'
+								type='email'
+								name='email'
+							/>
+						</div>
+						<div>
+							<label className='label'>Password</label>
+							<input
+								onChange={handleBusiness}
+								className='input'
+								type='password'
+								name='password'
+							/>
+						</div>
+
+						<div>
+							<label className='label'>Address</label>
+							<input
+								type='text'
+								name='address'
+								id='address'
+								onChange={handleBusiness}
+							/>
+						</div>
+
+						<div>
+							<label className='label'>Neighbor</label>
+							<input
+								type='text'
+								name='neighbor'
+								id='neighbor'
+								onChange={handleBusiness}
+							/>
+						</div>
+
+						<div>
+							<label className='label'>Cuisine</label>
+							<input
+								type='text'
+								name='cuisine'
+								id='cuisine'
+								onChange={handleBusiness}
+							/>
+						</div>
 					</>
-				)}
-			</form>
+					<button className='btn' type='submit' onClick={submitBusiness}>
+						Submit
+					</button>
+				</form>
+			) : null}
+			<button type='button' onClick={() => history.push("/")}>
+				Go home
+			</button>
 		</div>
 	);
 }
--- server/server.js
+++ server/server.js
@@ -10,62 +10,94 @@ const pathObj = path.parse(__dirname); //
 app.use(express.json());
 
 let clients = [];
+let business = [];
 
 const loadClients = () => {
-  const dataFile = fs.readFileSync(
-    `${pathObj.dir}/data/clientUsers.txt`,
-    "utf8"
-  );
+	const dataClient = fs.readFileSync(
+		`${pathObj.dir}/data/clientUsers.txt`,
+		"utf8"
+	);
 
-  const newDataFile = JSON.parse(dataFile);
+	const newDataClient = JSON.parse(dataClient);
 
-  clients = [...newDataFile];
+	clients = [...newDataClient];
 };
 
 const saveClients = () => {
-  fs.writeFileSync(
-    `${pathObj.dir}/data/clientUsers.txt`,
-    JSON.stringify(clients, null, "\t")
-  );
+	fs.writeFileSync(
+		`${pathObj.dir}/data/clientUsers.txt`,
+		JSON.stringify(clients, null, "\t")
+	);
+};
+const loadBusiness = () => {
+	const dataBusiness = fs.readFileSync(
+		`${pathObj.dir}/data/businessUsers.txt`,
+		"utf8"
+	);
+
+	const newDataBusiness = JSON.parse(dataBusiness);
+
+	business = [...newDataBusiness];
+};
+const saveBusiness = () => {
+	fs.writeFileSync(
+		`${pathObj.dir}/data/businessUsers.txt`,
+		JSON.stringify(business, null, "\t")
+	);
 };
 
 app.get("/restaurant", (req, res) => {
-  try {
-    res.send(menus);
-  } catch (e) {
-    console.log(e.message);
-  }
+	try {
+		res.send(menus);
+	} catch (e) {
+		console.log(e.message);
+	}
 });
 
-app.post("/register", (req, res) => {
-  try {
-    if (!req.body.client) return res.send({ success: false, errorId: 1 }); // username is empty
-    loadClients();
-    clients.push(req.body); // add the new user to the array
-    saveClients();
-    res.send({ success: true, client: req.body }); // send back to client the result
-  } catch (error) {
-    console.log("error in app.post", error.message);
-    res.send(error.message);
-  }
+app.post("/clients", (req, res) => {
+	console.log("Req Body", req.body);
+
+	try {
+		if (!req.body.client) return res.send({ success: false, errorId: 1 }); // username is empty
+		loadClients();
+		clients.push(req.body); // add the new user to the array
+		saveClients();
+		res.send({ success: true, client: req.body }); // send back to client the result
+	} catch (error) {
+		console.log("error in app.post", error.message);
+		res.send(error.message);
+	}
+});
+
+app.post("/business", (req, res) => {
+	try {
+		if (!req.body.business) return res.send({ success: false, errorId: 1 }); // username is empty
+		loadBusiness();
+		business.push(req.body); // add the new user to the array
+		saveBusiness();
+		res.send({ success: true, client: req.body }); // send back to client the result
+	} catch (error) {
+		console.log("error in app.post", error.message);
+		res.send(error.message);
+	}
 });
 const clientUsers = require("../data/clientUsers").clientUsers;
 app.get("/restaurant", (req, res) => {
-  try {
-    res.send(menus);
-  } catch (e) {
-    console.log(e.message);
-  }
-  console.log("restaurant is", menus);
+	try {
+		res.send(menus);
+	} catch (e) {
+		console.log(e.message);
+	}
+	console.log("restaurant is", menus);
 });
 
 app.get("/clientUsers", (req, res) => {
-  try {
-    res.send(clientUsers);
-  } catch (e) {
-    console.log(e.message);
-  }
-  console.log("client users are", clientUsers);
+	try {
+		res.send(clientUsers);
+	} catch (e) {
+		console.log(e.message);
+	}
+	console.log("client users are", clientUsers);
 });
 
 app.listen(port, () => console.log(`listening on port ${port}`));
