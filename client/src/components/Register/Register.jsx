b1cde2f23be578ca96f28cd5083604edcc10424b
Simona <semuona@gmail.com>
2022-02-22 12:52:51 +0100

    merged bussines and client data

--- client/src/components/Register/Register.jsx
+++ client/src/components/Register/Register.jsx
@@ -2,107 +2,131 @@ import { useState, useContext, useEffect } from "react";
 import { v4 as uuidv4 } from "uuid";
 import { HungryMeContext } from "../../Context";
 import axios from "axios";
+import RegisterForm from "./RegisterForm";
+import { Form, Col, Button, Row } from "react-bootstrap";
+import "./Register.scss";
 
 export default function Register() {
-	const { menu, setMenu, clientUser, setClientUser } =
-		useContext(HungryMeContext);
-	const [business, setBusiness] = useState(false);
-	const [client, setClient] = useState(false);
-	const [user, setUser] = useState({});
-	console.log(user);
-	console.log("Client ", client);
-	console.log("business", business);
+  const { menu, setMenu, clientUser, setClientUser } =
+    useContext(HungryMeContext);
+  const [business, setBusiness] = useState(false);
+  const [companyName, setCompanyName] = useState("");
+  const [cousineType, setCousineType] = useState("");
+  const [address, setAddress] = useState("");
 
-	const handleClient = (e) => {
-		e.preventDefault();
-		console.log("response is");
-		setClientUser({
-			...clientUser,
-			id: uuidv4(),
-			client: true,
-			favorites: [],
-			[e.target.name]: e.target.value,
-		});
-	};
+  const handleClient = (e) => {
+    e.preventDefault();
+    console.log("response is");
+    setClientUser({
+      ...clientUser,
+      id: uuidv4(),
+      favorites: [],
+      client: true,
+      [e.target.name]: e.target.value,
+      businessUser: business,
+      address: address,
+      companyName: companyName,
+      cousineType: cousineType,
+    });
+  };
 
-	const submitClient = async (e) => {
-		e.preventDefault();
+  const submitClient = async (e) => {
+    e.preventDefault();
 
-		const data = clientUser;
-		console.log("Submiting !!", data);
-		const response = await axios.post("/register", data);
-		console.log(response);
-	};
+    const data = clientUser;
+    console.log("Submitting !!", data);
+    const response = await axios.post("/register", data);
+    console.log("response is", response);
+  };
 
-	console.log(clientUser);
+  console.log(clientUser);
 
-	return (
-		<div>
-			<div>
-				<h1>User Registration</h1>
-			</div>
-			<input
-				type='radio'
-				value='business'
-				name='gender'
-				onChange={() => {
-					if (!business) {
-						setBusiness(true);
-						setClient(false);
-					}
-				}}
-			/>{" "}
-			Business owner
-			<input
-				type='radio'
-				value='client'
-				name='gender'
-				onChange={() => {
-					if (!client) {
-						setClient(true);
-						setBusiness(false);
-					}
-				}}
-			/>{" "}
-			Client
-			<form>
-				<label className='label'>Name</label>
-				<input
-					onChange={handleClient}
-					className='input'
-					name='username'
-					type='text'
-					id='name'
-				/>
-				<label className='label'>Email</label>
-				<input
-					onChange={handleClient}
-					className='input'
-					type='email'
-					name='email'
-				/>
-				<label className='label'>Password</label>
-				<input
-					onChange={handleClient}
-					className='input'
-					type='password'
-					name='password'
-				/>
-				{client ? (
-					<button className='btn' type='submit' onClick={submitClient}>
-						Submit
-					</button>
-				) : (
-					<>
-						<label className='label'>Address</label>
-						<input type='text' name='address' id='address' />
-						<label className='label'>Neighbor</label>
-						<input type='text' name='neighbor' id='neighbor' />
-						<label className='label'>Cuisine</label>
-						<input type='text' name='cuisine' id='cuisine' />
-					</>
-				)}
-			</form>
-		</div>
-	);
+  return (
+    <div className="registerForm">
+      <div>
+        <h1>User Registration</h1>
+      </div>
+      <input
+        type="radio"
+        value="client"
+        name="gender"
+        defaultChecked
+        onChange={() => {
+          setBusiness(false);
+
+          console.log("buisnes is from buisnes?", business);
+        }}
+      />{" "}
+      Client
+      <input
+        type="radio"
+        value="business"
+        name="gender"
+        onChange={() => {
+          setBusiness(true);
+          console.log("buisnes is from client?", business);
+        }}
+      />{" "}
+      Business owner
+      <form>
+        <label className="label">Name</label>
+        <input
+          onChange={handleClient}
+          className="input"
+          name="username"
+          type="text"
+          id="name"
+        />
+        <label className="label">Email</label>
+        <input
+          onChange={handleClient}
+          className="input"
+          type="email"
+          name="email"
+        />
+        <label className="label">Password</label>
+        <input
+          onChange={handleClient}
+          className="input"
+          type="password"
+          name="password"
+        />
+        {!business ? (
+          <button className="btn" type="submit" onClick={submitClient}>
+            Submit
+          </button>
+        ) : (
+          <>
+            <label className="label">Address</label>
+            <input
+              value={address}
+              onChange={(e) => setAddress(e.target.value)}
+              type="text"
+              name="address"
+              id="address"
+            />
+            <label className="label">Cousine Type</label>
+            <input
+              value={cousineType}
+              onChange={(e) => setCousineType(e.target.value)}
+              type="text"
+              name="cousine"
+              id="neighbor"
+            />
+            <label className="label">Company name</label>
+            <input
+              value={companyName}
+              type="text"
+              name="restaurant"
+              id="cuisine"
+              onChange={(e) => setCompanyName(e.target.value)}
+            />
+            <button className="btn" type="submit" onClick={submitClient}>
+              Submit
+            </button>
+          </>
+        )}
+      </form>
+    </div>
+  );
 }
